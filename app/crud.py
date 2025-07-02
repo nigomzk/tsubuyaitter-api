03c05431fb8cd164cf1e751472c011e5dd7cb01e
nigomzk <nigo3zuki@gmail.com>
2025-07-02 23:55:27 +0900

    authcodesテーブルのselectの戻り値の型をschemaに修正

--- app/crud.py
+++ app/crud.py
@@ -2,6 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import Authcode
+from app.schemas.auth import AuthcodeSchema
 
 
 async def insert_authcode(db: AsyncSession, email: str, code: str) -> Authcode:
@@ -29,11 +30,12 @@ async def insert_authcode(db: AsyncSession, email: str, code: str) -> Authcode:
     return authcode
 
 
-async def select_authcode_by_id(db: AsyncSession, authcode_id: str) -> Authcode | None:
+async def select_authcode_by_id(db: AsyncSession, authcode_id: str) -> AuthcodeSchema | None:
     """
     認証コードIDで認証コードを取得する。
 
     Returns:
         Authcode: 取得結果
     """
-    return (await db.scalars(select(Authcode).where(Authcode.authcode_id == authcode_id))).first()
+    result = (await db.scalars(select(Authcode).where(Authcode.authcode_id == authcode_id))).first()
+    return AuthcodeSchema(**result.__dict__) if result is not None else None
--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,4 +1,41 @@
+from datetime import datetime
+
 from pydantic import BaseModel, EmailStr, Field
+
+from app.core.config import get_settings
+
+
+class CommonFields:
+    """
+    共通フィールド
+    """
+
+    delete_flag: bool | None = Field(..., title="削除フラグ")
+    create_datetime: datetime | None = Field(..., title="作成日時")
+    update_datetime: datetime | None = Field(..., title="更新日時")
+
+
+class AuthcodeSchema(BaseModel, CommonFields):
+    """
+    認証コードスキーマ
+
+    Args:
+        BaseModel: Pydamic BaseModel
+        CommonFields: 共通フィールド
+    """
+
+    authcode_id: str = Field(..., min_length=36, max_length=36, title="認証コードID")
+    code: str = Field(
+        ...,
+        min_length=get_settings().AUTHCODE_LENGTH,
+        max_length=get_settings().AUTHCODE_LENGTH,
+        title="認証コード",
+    )
+    email: EmailStr = Field(..., title="メールアドレス")
+    expire_datetime: datetime = Field(..., title="有効期限")
+
+    class ConfigDict:
+        from_attributes = True
 
 
 class RequestIssueAuthcodeForEmail(BaseModel):
@@ -14,5 +51,5 @@ class ResponseIssueAuthcodeForEmail(BaseModel):
     メール認証コード発行レスポンス
     """
 
-    authcode_id: str = Field(..., min_length=1, max_length=36, title="認証コードID")
+    authcode_id: str = Field(..., min_length=36, max_length=36, title="認証コードID")
     expire_datetime: str = Field(..., title="有効期限")
