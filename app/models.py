0aa0ec66d5dfdb766f27144fc051ea10050b090e
nigomzk <nigo3zuki@gmail.com>
2025-07-05 23:08:04 +0900

    refactor: #2 mapped_columnに移行
    SQLAlchemyのColumn型をmapped_columnに移行しました。
    
    Closes #2

--- app/models.py
+++ app/models.py
@@ -1,43 +1,52 @@
 import uuid
 from datetime import datetime, timedelta
 
-from sqlalchemy import Boolean, Column, DateTime, String
+from sqlalchemy import Boolean, DateTime, String
+from sqlalchemy.orm import Mapped, declared_attr, mapped_column
 
 from app.core.config import get_settings
 from app.core.database import Base
 
 
-class CommonColumns:
+class BaseModelMixin:
     """
-    共通カラム
+    基底モデル
     """
 
-    delete_flag = Column(Boolean, default=False, nullable=False, comment="削除フラグ")
-    create_datetime = Column(DateTime, default=datetime.now(), nullable=False, comment="作成日時")
-    update_datetime = Column(
-        DateTime,
-        default=datetime.now(),
-        onupdate=datetime.now(),
-        nullable=False,
-        comment="更新日時",
-    )
+    @declared_attr
+    def delete_flag(cls) -> Mapped[bool]:
+        return mapped_column(Boolean, default=False, nullable=False, comment="削除フラグ")
+
+    @declared_attr
+    def create_datetime(cls) -> Mapped[datetime]:
+        return mapped_column(DateTime, default=datetime.now(), nullable=False, comment="作成日時")
+
+    @declared_attr
+    def update_datetime(cls) -> Mapped[datetime]:
+        return mapped_column(
+            DateTime,
+            default=datetime.now(),
+            onupdate=datetime.now(),
+            nullable=False,
+            comment="更新日時",
+        )
 
 
-class Authcode(Base, CommonColumns):
+class Authcode(Base, BaseModelMixin):
     """
-    認証コード
+    認証コードモデル
     """
 
     __tablename__ = "authcodes"
-    authcode_id = Column(
+    authcode_id: Mapped[str] = mapped_column(
         String(36),
         primary_key=True,
         default=lambda: str(uuid.uuid4()),
         comment="認証コードID",
     )
-    code = Column(String(6), nullable=False, comment="コード")
-    email = Column(String(255), nullable=False, comment="メールアドレス")
-    expire_datetime = Column(
+    code: Mapped[str] = mapped_column(String(6), nullable=False, comment="コード")
+    email: Mapped[str] = mapped_column(String(255), nullable=False, comment="メールアドレス")
+    expire_datetime: Mapped[datetime] = mapped_column(
         DateTime,
         default=(datetime.now() + timedelta(minutes=get_settings().AUTHCODE_EXPIRE_MINUTES)),
         nullable=False,
